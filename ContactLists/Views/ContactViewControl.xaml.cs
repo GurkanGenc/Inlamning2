b2f266502ffa207dfa02b2faf530440d1cc34d9e
Gürkan Genç <wishblood@gmail.com>
2020-09-21 11:27:12 +0200

    Add project files.

---
+++ ContactLists/Views/ContactViewControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContactLists.Views
+{
+    /// <summary>
+    /// Interaction logic for ContactViewControl.xaml
+    /// </summary>
+    public partial class ContactViewControl : UserControl
+    {
+        public ContactViewControl()
+        {
+            InitializeComponent();
+        }
+
+        private void contactButton_Click(object sender, RoutedEventArgs e)
+        {
+            contactImage.Source = new BitmapImage(new Uri("/Images/Angelina_Jolie.jpg", UriKind.Relative));
+            contactName.Text = "Angelina Jolie";
+            occupation.Text = "Movie Star";
+            DataContext = new ContactViewControl();
+        }
+    }
+}
---
+++ ContactLists/Views/MessageViewControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContactLists.Views
+{
+    /// <summary>
+    /// Interaction logic for MessageViewControl.xaml
+    /// </summary>
+    public partial class MessageViewControl : UserControl
+    {
+        public MessageViewControl()
+        {
+            InitializeComponent();
+        }
+
+        private void messageButton_Click(object sender, RoutedEventArgs e)
+        {
+            time.Text = DateTime.Now.ToString();
+            subject.Text = "Have you seen Brad??";
+            from.Text = "Angelina Jolie";
+            to.Text = "a.jolie1974@gmail.com";
+            body.Text = "Vestibulum interdum eros sed congue pellentesque. Ut ac purus nec leo efficitur imperdiet ut vel est. Praesent consequat turpis id turpis rhoncus condimentum. Nam quis vehicula purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris volutpat lacus sed felis convallis scelerisque. Vestibulum aliquam finibus ultricies. Praesent eleifend porta metus, nec laoreet mauris vulputate et. Vestibulum tincidunt nulla ac quam facilisis congue.";
+        }
+    }
+}
